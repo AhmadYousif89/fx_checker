73a3204b4e4d5344b620fed3b35fb4b4c93d0ce3
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2026-07-14 15:59:38 +0300

    fix: ensure CustomSpinner div takes full height and grows appropriately

--- src/components/custom-spinner.tsx
+++ src/components/custom-spinner.tsx
@@ -1,6 +1,6 @@
 export const CustomSpinner = () => {
   return (
-    <div className="flex items-center justify-center py-10">
+    <div className="h-full grow flex items-center justify-center py-10">
       <span className="size-20 md:size-40 rounded-full aspect-square border-b-2 border-accent animate-spin duration-100" />
     </div>
   )
